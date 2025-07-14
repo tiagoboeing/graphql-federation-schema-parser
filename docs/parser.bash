2a32b0e7d82816d038ac14b47cb5b1038b3298cf
Tiago Boeing <contato@tiagoboeing.com>
2025-07-14 14:12:28 -0300

    feat: add recursive schema parsing and improve documentation
    - Add support for parsing nested directories with GraphQL schema files
    - Add debug mode for troubleshooting with detailed logs
    - Add documentation on CLI usage, commands, and debug mode
    - Add terminal screenshot and feature table to README
    - Include example schema in subfolder for testing
    - Add start:debug script for development

---
+++ docs/parser.bash
@@ -0,0 +1,22 @@
+➜  ./gql-federation-schema-parser --help                           
+   _____  ____  _         _____      _                            _____                         
+  / ____|/ __ \| |       / ____|    | |                          |  __ \                        
+ | |  __| |  | | |      | (___   ___| |__   ___ _ __ ___   __ _  | |__) |_ _ _ __ ___  ___ _ __ 
+ | | |_ | |  | | |       \___ \ / __| '_ \ / _ \ '_ ` _ \ / _` | |  ___/ _` | '__/ __|/ _ \ '__|
+ | |__| | |__| | |____   ____) | (__| | | |  __/ | | | | | (_| | | |  | (_| | |  \__ \  __/ |   
+  \_____|\___\_\______| |_____/ \___|_| |_|\___|_| |_| |_|\__,_| |_|   \__,_|_|  |___/\___|_|   
+                                                                                                
+                                                                                                
+Usage: gql-federation-schema-parser [options] [command]
+
+CLI tool to parse and manipulate GraphQL schemas for federation
+
+Options:
+  -V, --version    output the version number
+  -D --debug       Enable debug mode (default: false)
+  -S --simple      Disable colors on terminal (default: false)
+  -h, --help       display help for command
+
+Commands:
+  parse [options]  Parse GraphQL schemas
+  help [command]   display help for command
